04695c83fd52f696f2155bd84f5f4c530a59cf35
Saransh <saransh.code@gmail.com>
2023-11-20 07:51:32 +0530

    Fisrt commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Work from "./components/Work";
+ 
+function App() {
+  return (
+    <div>
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Skills/>
+        <Work/>
+        <Contact/> 
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+import WorkIm from '../assets/projects/crpto.png'
+import RealEst from '../assets/projects/food.png'
+import Videoapp from '../assets/projects/video.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Videoapp,
+        github:"",
+        live:"",
+    },
+ 
+
+
+]
